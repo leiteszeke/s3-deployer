41f523f27ed502c61b6e0d152b2e93b9e58b261f
Ezequiel Leites <ezequiel.leites@ext.privalia.com>
2019-10-16 09:54:58 +0200

    New script

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Testing the deployer
+          Probando el nuevo script
         </a>
       </header>
     </div>
